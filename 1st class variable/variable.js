027de3dc590a96516791f18a57dcc3b43aec5f1d
Rialahmed0 <rashedahommod0@gmail.com>
2023-12-13 09:18:30 +0600

    variable

---
+++ 1st class variable/variable.js
@@ -0,0 +1,18 @@
+/* 
+1) variable type 
+   i) numaric varible 
+   ii) string variable 
+   iii) null variable
+   iv) boolean variable
+
+2) math operator = + , - , * , / ;
+
+3)shortHand = += , -=, ++ , -- ;
+
+4)convert = parseFloat , parsesInt ;
+
+5) modulas operator (%)
+
+6) tofix ;
+
+*/
