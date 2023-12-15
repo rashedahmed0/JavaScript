4356e3c299ff1802daa34805f445b1f0fc008283
Rialahmed0 <rashedahommod0@gmail.com>
2023-12-15 09:32:16 +0600

    condition

---
+++ 3rd class condition/comparism.js
@@ -0,0 +1,11 @@
+// check equality 
+console.log('check equality');
+console.log(5 == 5);
+console.log(5 == '5');
+console.log('rial' == 'ahmed');
+console.log(7 != 2);
+console.log(7 !== '7');
+console.log(7 > 3);
+console.log(6 < 10);
+console.log(5 > 9);
+console.log(5 < 4);
