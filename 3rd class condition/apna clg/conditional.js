d62dc4229a6c7e1a060ee5dd925f486771df0023
Rialahmed0 <rashedahommod0@gmail.com>
2023-12-16 10:19:35 +0600

    conditional

---
+++ 3rd class condition/apna clg/conditional.js
@@ -0,0 +1,53 @@
+//conditional statement 
+// let age = 15;
+
+// if (age > 18) {
+//     console.log('you can vote !');
+// }
+// if (age < 18) {
+//     console.log('you can not vote !');
+// }
+// let mode = 'light';
+// let color;
+
+// if (mode === 'dark') {
+//     color = 'black';
+// }
+// if (mode === 'light') {
+//     color = 'white';
+
+// }
+// console.log(color);
+
+
+
+// let num = 1; 
+
+// if (num % 2 === 0) {
+//     console.log(num + ' is even');
+// }
+// else {
+//     console.log(num + ' is odd ');
+// }
+
+// let mode = 'silver';
+// let color;
+// if (mode === 'dark') {
+//     color = 'black';
+// }
+// else if (mode === 'blue') {
+//     color = 'blue';
+// }
+// else if (mode === 'light') {
+//     color = 'white';
+
+// }
+// else {
+//     color = 'red';
+
+// }
+// console.log(color);
+
+let age = 20;
+age >= 18 ? console.log('adult') : console.log("not adult");
+console.log(resutl);
---
+++ 3rd class condition/apna clg/script.js
@@ -0,0 +1,26 @@
+// // artimetic Operators 
+// let a = 5;
+// let b = 2;
+
+// console.log('a = ', a, '&', 'b = ', b); 
+// console.log("a + b :", a + b);
+// console.log("a - b :", a - b);
+// console.log("a * b :", a * b);
+// console.log("a / b :", a / b);
+// console.log("a % b :", a % b);
+// console.log("a ** b :", a ** b);
+// console.log("a ++ :", a++);
+// console.log("a -- :", a--);
+
+
+// a = a + 1;
+// console.log('a = ', a);
+// a++;
+// console.log('a =', a);
+
+let a = 5;
+let b = 2;
+
+let cond1 = a !== b; //true ;
+let cond2 = b !== a; // true ;
+// console.log('cond1 && cond2 =', cond1 && cond2);
