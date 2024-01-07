aa28269a5fc6104f48c353bac883faf25d77d0f0
Rialahmed0 <rashedahommod0@gmail.com>
2024-01-07 10:30:53 +0600

    new

---
+++ 6th function/apnaclg/arrowFunction.js
@@ -0,0 +1,33 @@
+// sum 
+// function sum(a, b) {
+//     s = a + b;
+//     return s
+
+// }
+// function mul(a, b) {
+//     m = a * b;
+//     return m;
+// }
+
+
+const arrowsum = (a, b) => {
+    s = a + b;
+    return s
+}
+arrowsum(10, 10);
+console.log(s);
+
+
+
+const mul = (a, b) => {
+    s = a * b
+    console.log(s);
+
+}
+mul(5, 5)
+
+const printHello = () => {
+    console.log("hello");
+}
+
+
---
+++ 6th function/apnaclg/script.js
@@ -0,0 +1,20 @@
+/*function functionName(){
+    body
+}
+functionName()*/
+
+function myfunction(msg, num) {
+    // console.log(msg, num);
+
+}
+myfunction('this is my msg', 100)
+
+// sum
+// function sum(a, b) {
+//     add = a + b;
+//     return add;
+
+
+// }
+// sum(10, 5)
+// console.log(add);
