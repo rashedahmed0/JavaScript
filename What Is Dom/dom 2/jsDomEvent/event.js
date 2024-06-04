97b8885f26d5ac0d97d587f65d7c3290a6eb54e5
Rialahmed0 <rashedahommod0@gmail.com>
2024-06-04 21:28:02 +0600

    event

--- What Is Dom/dom 2/jsDomEvent/event.js
+++ What Is Dom/dom 2/jsDomEvent/event.js
@@ -1,1 +1,36 @@
 console.log('hello world');
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+const makeTomatoButton = document.getElementById('make-tomato');
+makeTomatoButton.onclick = function makeTomato() {
+    document.body.style.backgroundColor = 'tomato';
+}
+
+const makePinkButton = document.getElementById("make-pink");
+makePinkButton.addEventListener('click', makePink);
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.addEventListener('click', function makePurple() {
+    document.body.style.backgroundColor = 'purple'
+})
+
+
+document.getElementById('make-orange').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'orange';
+})
