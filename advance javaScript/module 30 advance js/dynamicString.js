1e4e1cbdcef80c1c81e1b24eb688556d95ce60c1
Rialahmed0 <rashedahommod0@gmail.com>
2024-08-07 02:54:40 +0600

    dynamic string

---
+++ advance javaScript/module 30 advance js/dynamicString.js
@@ -0,0 +1,32 @@
+// dynamic string
+
+// 1
+let name = 'rial';
+let greating = `hello ${name}`
+console.log(greating);
+
+// 2 
+let num1 = 5;
+let num2 = 6;
+let sum = `${num1} and ${num2} sum is : ${num1 + num2} `;
+console.log(sum);
+
+// 2
+let multyLine = `
+amr sonar bangla 
+ami tomay valobasi 
+chirodin tmr akas tmr batas 
+amr prane bajay basi 
+`
+console.log(multyLine);
+
+// 3 
+let title = 'this is title';
+let body = 'this is body of this articale'
+let html = `
+<article>
+<h1>${title}</h1>
+<p>${body}</p>
+</article>
+`;
+console.log(html);
