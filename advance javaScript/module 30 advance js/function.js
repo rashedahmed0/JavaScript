f00f5ed11fe3c5d7d21e0c80559fb72b1bf2fee6
Rialahmed0 <rashedahommod0@gmail.com>
2024-08-07 02:13:13 +0600

    deafult function parameter

---
+++ advance javaScript/module 30 advance js/function.js
@@ -0,0 +1,31 @@
+// 1
+function add(num1 = 3, num2 = 1) {
+    let sum = num1 + num2;
+    console.log(num1, num2, sum);
+
+}
+add(7, 9)
+// add(9)
+// add()
+
+
+// 2
+function str(firstName, lastName = ' ') {
+
+}
+
+
+// 3 
+function arr(array = []) {
+
+}
+
+// 4 
+function obj(object = {}) {
+
+}
+
+// 5 
+function bool(boolean = ture) {
+
+}
