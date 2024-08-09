5983581e4dc61fe3c5d43278ab8842ba8d13d10a
Rialahmed0 <rashedahommod0@gmail.com>
2024-08-09 21:50:46 +0600

    distructure

--- advance javaScript/module 30 advance js/destructure.js
+++ advance javaScript/module 30 advance js/destructure.js
@@ -1,43 +1,61 @@
-//object 
+// object 
 let person = {
     name: 'rial',
     age: 24,
-    department: 'cse',
-    CGPA: 4.54
-}
-let { name, age, department, } = person;
-console.log(name);
-console.log(age);
-console.log(department);
-
-let employee = {
-    position: 'web developer',
-    salary: 50000,
+    isStudent: true,
+    isMarid: false
 
 }
-let { position, salary } = employee;
-console.log(position);
-console.log(salary);
+
+console.log(person);
+console.log('person name is : ', person.name);
+console.log('person age is : ', person['age']);
+let student = 'isStudent';
+console.log('is person student ? : ', person[student]);
+
+let keys = Object.keys(person)
+let values = Object.values(person);
+console.log('person objects keys is :', keys);
+console.log('person objects values is :', values);
+
+// for is 
+for (let key in person) {
+    console.log(key, person[key]);
+}
+
+// for 
+for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    console.log(key, person[key]);
+}
+
+// for of 
+for (let key of keys) {
+    console.log(key, person[key]);
+}
+
+// destructure 
+let { name, age, isMarid, isStudent } = person
+let myName = name;
+let myAge = age;
+console.log("my name is :", myName);
+console.log('my age is : ', myAge);
+console.log('marid status ;', isMarid);
+console.log('professional status ;', isStudent);
 
 
-// array 
-console.log('number1');
-let number1 = [1, 2, 3, 4, 56];
-let [first, second, third, fourth] = number1;
+
+let arr = [1, 2, 3, 4, 45];
+// let [first, second, ...rest] = arr
+let [first, , third, ...rest] = arr
 console.log(first);
-console.log(second);
+// console.log(second);
 console.log(third);
-console.log(fourth);
+console.log(...rest);
 
+function add(num1, num2) {
+    return [num1, num2];
 
-console.log('number2');
-let number2 = [1, 2, 3, 4, 5, 6, 7];
-let [st1, , rd3, th4] = number2;
-console.log(st1);
-console.log(rd3);
-console.log(th4);
-
-
-
-
-
+}
+let [a, b] = add(5, 10);
+console.log(a, b);
