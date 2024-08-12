168a545099c3b1d37e927e13808df967a78f4dd1
Rialahmed0 <rashedahommod0@gmail.com>
2024-08-12 21:21:32 +0600

    dynamic string

--- advance javaScript/module 31 ES6/recap module 30/string.js
+++ advance javaScript/module 31 ES6/recap module 30/string.js
@@ -1,1 +1,11 @@
-let 
+let number1 = 5;
+let number2 = 3;
+let sum = number1 + number2;
+console.log('normal print : ' + number1 + " " + number2 + " sum is : " + sum);
+console.log(`dynamic print ${number1} and ${number2} sum is : ${sum}`);
+let multiLine = `hello everyone
+myself rashed ahmed 
+i am a web developer 
+`;
+
+console.log(multiLine);
