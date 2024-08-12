a6959a29a441c36308b91a3d3b543b32aad2c531
Rialahmed0 <rashedahommod0@gmail.com>
2024-08-12 19:57:12 +0600

    default parameter function

--- advance javaScript/module 31 ES6/recap module 30/default.js
+++ advance javaScript/module 31 ES6/recap module 30/default.js
@@ -1,18 +1,65 @@
-function add(num1, num2) {
-    let sum = num1 + num2;
-    console.log(sum);
-}
-add(5, 5)
-
-function sum(a, b = 0) {
+// number 
+function add(a, b) {
     let sum = a + b;
     console.log(a, b, sum);
 }
-sum(5,)
-
-function str(firstName, lastName = " ") {
-    let fullName = firstName + lastName;
-    console.log(firstName, lastName, fullName);
+add(5, 10);
+// number default funtion parameter 
+function add2(a, b = 0) {
+    let sum = a + b;
+    console.log(a, b, sum);
 }
-// str('rial', 'ahmed')
-str('rial')
+
+add2(2)
+
+
+// string 
+function str(firstName, lastName) {
+    let fullName = firstName + lastName;
+    console.log(firstName, lastName, 'full name is :', fullName);
+}
+str('rial', 'ahmed')
+
+// string default function parameter 
+function str2(firstName, lastName = '') {
+    let fullName = firstName + lastName;
+    console.log(firstName, lastName, 'full name is : ', fullName);
+}
+str2('rashed')
+
+// array 
+function arr(arr1, arr2) {
+    console.log(arr1, arr2);
+}
+arr([1, 3, 5], [2, 4, 6]);
+
+// array default function parameter 
+function arr2(arr1, arr2 = []) {
+    console.log(arr1, arr2);
+}
+arr2([1, 2, 3, 4, 5, 6]);
+
+// object 
+function obj(obj1, obj2) {
+    console.log(obj1, obj2);
+}
+let friend1 = {
+    name: 'rial',
+    age: 'dosnt matter'
+}
+let friend2 = {
+    name: 'rashed',
+    age: 24
+}
+obj(friend1, friend2);
+
+
+// object default function parameter
+function obj2(obj1, obj2 = {}) {
+    console.log(obj1, obj2);
+}
+let friend3 = {
+    name: 'ahmed',
+    age: 24
+}
+obj2(friend3);
