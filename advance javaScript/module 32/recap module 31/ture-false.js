0eaf0b3e7b83fbd505127e4df7ea1f5e5417d3ba
Rialahmed0 <rashedahommod0@gmail.com>
2024-09-03 21:48:09 +0600

    truty value and falsy value

---
+++ advance javaScript/module 32/recap module 31/ture-false.js
@@ -0,0 +1,120 @@
+// falsy values
+
+
+// 1
+let isFalse = false;
+if (isFalse) {
+    console.log('value of isFalse is truthy ');
+}
+else {
+    console.log('value of isFalse is flasy');
+}
+
+
+// 2 
+let zero = 0;
+if (zero) {
+    console.log('value of zero is truthy');
+}
+else {
+    console.log('value of zero is falsy');
+}
+
+// 3
+let emptyString = '';
+if (emptyString) {
+    console.log('value of emptystring is truthy ');
+}
+else {
+    console.log('value of empty string is flasy');
+}
+
+
+// 4 
+let nul = null;
+if (nul) {
+    console.log('values of null is truthy ');
+}
+else {
+    console.log('values of null is falsy ');
+}
+
+
+// 5 
+let undfnd = undefined;
+if (undfnd) {
+    console.log('values of undefined is truthy');
+}
+else {
+    console.log('values of undefined is falsy');
+}
+
+
+//6
+let nan = NaN;
+if (nan) {
+    console.log('values of  NaN is truthy  ');
+}
+else {
+    console.log('values of NaN is falsy ');
+}
+
+
+
+
+
+
+
+
+// truthy values
+
+
+//1
+let isTrue = true;
+if (isTrue) {
+    console.log('value of true is  truthy');
+}
+else {
+    console.log('values of true is  flasy');
+}
+
+
+// 2
+let number = 20;
+if (number) {
+    console.log('value of number is truthy');
+}
+else {
+    console.log('values of number is falsy ');
+}
+
+// 3
+
+let str = 'hello every one';
+if (str) {
+    console.log('value of str is truthy');
+}
+else {
+    console.log('value of str is falsy');
+}
+
+// 4
+// let obj = { name: 'rial', age: 24 };
+let obj = {};
+if (obj) {
+    console.log('values of obj is truthy');
+}
+else {
+    console.log('values of obj is falsy');
+}
+
+// 5 
+// let arr = [1, 2, 3];
+let arr = [];
+if (arr) {
+    console.log('value of arr is turthy');
+}
+else {
+    console.log('value of arr is falsy');
+}
+
