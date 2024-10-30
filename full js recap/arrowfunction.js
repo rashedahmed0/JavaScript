1180ea912e039de1e9b58392839f09c2ab2d6822
Rialahmed0 <rashedahommod0@gmail.com>
2024-10-30 20:23:42 +0600

    arrow function

--- full js recap/arrowfunction.js
+++ full js recap/arrowfunction.js
@@ -4,3 +4,26 @@ function sum1(a, b) {
 }
 let number1 = sum1(5, 2);
 console.log(number1);
+// arrow function 
+let sum2 = (a, b) => a + b;
+let number2 = sum2(3, 9);
+console.log(number2);
+
+function message1(message) {
+    console.log(message);
+}
+message1('lets make something big')
+
+let message = message => console.log(message);
+message('lets make some history')
+
+
+
+function filterEven(number) {
+    let element = number;
+    if (element % 2 === 0) {
+        console.log(element);
+    }
+}
+let arr = [1, 3, 4, 5];
+let numbers = filterEven(arr)
