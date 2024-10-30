119a0de17c23f56bba20052d600206817f144bed
Rialahmed0 <rashedahommod0@gmail.com>
2024-10-30 19:16:23 +0600

    dynamic stirng

---
+++ full js recap/arrowfunction.js
@@ -0,0 +1,6 @@
+function sum1(a, b) {
+    return a + b;
+
+}
+let number1 = sum1(5, 2);
+console.log(number1);
---
+++ full js recap/dynamicstring.js
@@ -0,0 +1,10 @@
+let firstName = 'rial';
+let lastName = ' ahmed';
+
+
+let fullName = `full name is : ${firstName + lastName} `
+console.log(fullName);
+
+let num1 = 5;
+let num2 = 2;
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
