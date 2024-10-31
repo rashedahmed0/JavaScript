68ecdb9dd16e9ca794f668dbb2e1a7f6f291f58e
Rialahmed0 <rashedahommod0@gmail.com>
2024-10-31 08:43:13 +0600

    class in js

---
+++ full js recap/class.js
@@ -0,0 +1,31 @@
+class person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    // method
+    greet() {
+        console.log(`hi everyone this is ${this.name} . I am ${this.age} years old`);
+    }
+}
+let person1 = new person('rial', 24);
+person1.greet()
+
+class employee {
+    constructor(name, age, address, salary) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    about() {
+        let myself = `hi this is ${this.name} , i am ${this.age} years old and , i live in ${this.address} city , my current salary is ${this.salary}`
+        console.log(myself);
+
+    }
+}
+let employee1 = new employee('rial', 24, 'dhaka', 25000);
+employee1.about()
+let employee2 = new employee('dinar', 34, 'dhaka', 100000);
+employee2.about()
