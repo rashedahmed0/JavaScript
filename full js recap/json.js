561da11c4ca684e712af1afbff1b5294929ebb7d
Rialahmed0 <rashedahommod0@gmail.com>
2024-10-31 18:56:14 +0600

    json

--- full js recap/json.js
+++ full js recap/json.js
@@ -4,4 +4,19 @@ let person = {
     isStudent: true,
     isMarrid: false
 }
-console.log(person.name);
+console.log(person);
+let jsonPerson = JSON.stringify(person);
+console.log(jsonPerson);
+let objPerson = JSON.parse(jsonPerson)
+console.log(objPerson);
+
+let employee = {
+    name: 'rashed',
+    age: 25,
+    isEmployee: true,
+    salary: 25000
+}
+let jsonEmployee = JSON.stringify(employee);
+console.log(jsonEmployee);
+let objEmp = JSON.parse(jsonEmployee)
+console.log(objEmp);
