10ea3403a96f88fb8c6e10f27301a21d50197c5f
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-01 20:59:45 +0600

    variable

---
+++ full js recap/re recap/variable.js
@@ -0,0 +1,22 @@
+let name = 'rial';
+let age = 25;
+let isMarid = false;
+let isStudent = true;
+// console.log(name);
+// console.log(age);
+// console.log(isMarid);
+// console.log(isStudent);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isMarid);
+console.log(typeof isStudent);
+
+
+// method
+let number = '200.3458';
+let floatNumber = parseFloat(number);
+let digit2Number = floatNumber.toFixed(3);
+let intNumber = parseInt(digit2Number);
+console.log(typeof intNumber, intNumber);
+
