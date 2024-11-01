6103d8eb8bdfaa2826afcc8271bc04c6390856fa
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-01 21:12:53 +0600

    array

---
+++ full js recap/re recap/array.js
@@ -0,0 +1,14 @@
+let ages = [13, 42, 33, 42, 51];
+console.log(ages);
+let agesLength = ages.length;
+console.log(agesLength);
+let indexOf33 = ages.indexOf(33);
+console.log(indexOf33);
+let firstAge = ages[0];
+console.log(firstAge);
+ages[0] = 24;
+ages.pop()
+ages.shift()
+ages.unshift(39)
+ages.push(24)
+console.log(ages);
---
+++ full js recap/re recap/operator.js
@@ -0,0 +1,4 @@
+// arithmathic operator 
+let num1 = 5;
+let num2 = 2;
+console.log(object);
