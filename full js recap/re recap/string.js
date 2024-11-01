3ec50f995b54d03fc2cbd0e19013cfe369381abf
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-01 22:29:37 +0600

    string

---
+++ full js recap/re recap/string.js
@@ -0,0 +1,20 @@
+let str = 'hello everyone';
+let capi = str.toUpperCase();
+console.log(capi);
+let small = str.toLowerCase()
+console.log(small);
+
+// search case 
+let hello = str.includes('on');
+console.log(hello);
+
+let one = str.indexOf('one');
+console.log(one);
+
+// split case 
+let splitstr = str.split(' ');
+console.log(splitstr);
+
+let sliceStr = str.slice(0, 5);
+console.log(sliceStr);
+
