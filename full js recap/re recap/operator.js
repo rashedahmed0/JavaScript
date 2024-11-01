0613e9151e57838c677d2eaea78844dd11f064d1
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-01 21:21:25 +0600

    operator

--- full js recap/re recap/operator.js
+++ full js recap/re recap/operator.js
@@ -1,4 +1,19 @@
 // arithmathic operator 
 let num1 = 5;
 let num2 = 2;
-console.log(object);
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 / num2);
+// console.log(num1 * num2);
+// console.log(num1 ** num2);
+// console.log(num1 % num2);
+
+// assign operator 
+console.log(num1 += 2);
+console.log(num1 -= 2);
+console.log(num1 /= 2);
+console.log(num1 *= 2);
+console.log(num1 **= 2);
+console.log(num1 %= 2);
+
+// logic operator 
