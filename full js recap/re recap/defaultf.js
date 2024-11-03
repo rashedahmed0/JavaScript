4f968e9480b8d1839df476421beaca888b71533f
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-03 11:03:22 +0600

    defaoult function value

---
+++ full js recap/re recap/defaultf.js
@@ -0,0 +1,23 @@
+function number(a, b = 0) {
+
+}
+number(1)
+
+
+
+function str(firstName, lastName = '') {
+
+}
+str('rashed');
+
+function arr(arr1, arr2 = []) {
+
+}
+arr([1, 2, 3, 4, 5]);
+
+function obj(obj1, obj2 = {}) {
+
+}
+
+obj({ name: 'rial', age: 24 });
+
