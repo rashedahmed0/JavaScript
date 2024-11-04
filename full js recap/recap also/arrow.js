92b7eb149f0d017981a4c75f2aa3c21de56ce8a5
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-04 22:57:25 +0600

    arrow function

---
+++ full js recap/recap also/arrow.js
@@ -0,0 +1,11 @@
+let sum = (a, b) => a + b;
+let number = sum(5, 2);
+console.log(number);
+
+let square = (x) => x ** 2;
+let number1 = square(5);
+console.log(number1);
+
+let greet = () => 'hello everyOne ';
+console.log(greet());
+
