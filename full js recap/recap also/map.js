31e27727e7e5d7b1afa121ecb2728e021c2f2d36
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-04 22:46:23 +0600

    map , forEach , filter , find

--- full js recap/recap also/map.js
+++ full js recap/recap also/map.js
@@ -1,5 +1,5 @@
 let arr = [1, 2, 3, 4, 5];
-let arrow = (a) => console.log(a + 5);
+let arrow = (a) => a % 2 === 0;
 // let arrowMap = arr.map(arrow)
 // console.log(arrowMap);
 // let map = arr.map((a) => a * 3);
@@ -9,5 +9,11 @@ let arrow = (a) => console.log(a + 5);
 // let forEach = arr.forEach(a => a + 10)
 // console.log(forEach);
 
+// let arrowFilter = arr.filter(arrow);
+// console.log(arrowFilter);
+// let filter = arr.filter(a => a % 2 === 1);
+// console.log(filter);
 
+let find = arr.find(a => a % 2 === 1)
+console.log(find);
 
