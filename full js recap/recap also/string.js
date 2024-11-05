4202ffebf369158104f3284aa19c46c581edfce3
Rialahmed0 <rashedahommod0@gmail.com>
2024-11-06 00:05:22 +0600

    string

---
+++ full js recap/recap also/string.js
@@ -0,0 +1,11 @@
+let greet = 'hello everyOne';
+console.log(greet);
+let sliceGreet = greet.slice(2, 4);
+console.log(sliceGreet);
+let splitGreet = greet.split(' ')
+console.log(splitGreet);
+
+let includesGreet = greet.includes('hello');
+let indexGreet = greet.indexOf('One');
+console.log(includesGreet);
+console.log(indexGreet);
