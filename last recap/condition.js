dee2116c8c22c7e1e23cd4bd706b93d2b96d4fd6
Rialahmed0 <rashedahommod0@gmail.com>
2025-01-20 11:01:08 +0600

    condition

---
+++ last recap/condition.js
@@ -0,0 +1,32 @@
+// operator 
+let number1 = 10;
+let number2 = 5;
+
+let addition = number1 + number2;
+console.log(addition);
+let minus = number1 - number2;
+let divided = number1 / number2;
+let multiple = number1 * number2;
+
+// assaignment opertor 
+
+let number = 20;
+number += 5;
+number *= 2;
+number /= 5;
+number -= 20;
+console.log(number);
+
+
+//  condition 
+if (number1 > number2) {
+    console.log('number 1 is greater then number2 ');
+
+
+}
+else {
+    console.log('number 2 is greater then number 1');
+}
+
+// ternary operator 
+let greaterNumber = (number1 < number2) ? console.log('number 2 is greater than number 1 ') : console.log('nubmer 1 is greater then number 2 ');
