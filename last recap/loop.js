5458a005fc6db385557fb46051213aca37a4652b
Rialahmed0 <rashedahommod0@gmail.com>
2025-01-20 11:22:48 +0600

    loop

---
+++ last recap/loop.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 10; i++) {
+    // console.log(i);
+}
+let numbers = [1, 2, 3, 4, 5];
+for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    // console.log(element);
+}
+for (let number of numbers) {
+    // console.log(number);
+}
+
