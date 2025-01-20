10f745a92e61fd99aacbcf1f4d1543c93e86530b
Rialahmed0 <rashedahommod0@gmail.com>
2025-01-20 10:21:18 +0600

    array

---
+++ last recap/array.js
@@ -0,0 +1,20 @@
+let numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+// length 
+console.log(numbers.length);
+// get value 
+let firstElement = numbers[0]
+console.log(firstElement);
+// set value 
+numbers[1] = 11;
+console.log(numbers);
+// indexof
+console.log(numbers.indexOf(5));
+// push pop
+numbers.pop()
+let push10 = numbers.push(10);
+console.log(numbers);
+//shift unshift
+numbers.shift();
+numbers.unshift(255);
+
