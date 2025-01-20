bbcfaa3272877cde0b9b2f509295d5c8c1a998c2
Rialahmed0 <rashedahommod0@gmail.com>
2025-01-20 10:03:01 +0600

    variable

---
+++ last recap/var.js
@@ -0,0 +1,12 @@
+var name = 'rial ';
+var age = 24;
+var isStudent = true;
+var isEmployee = false;
+
+
+var number = '245.848928843';
+var floatNumber = parseFloat(number)
+var digit2Number = floatNumber.toFixed(2);
+var intNumber = parseInt(digit2Number);
+
+console.log(intNumber);
