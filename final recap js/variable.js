3e2b80888475f38237e036374316bfa338983115
Rialahmed0 <rashedahommod0@gmail.com>
2025-02-12 22:05:05 +0600

    variable

---
+++ final recap js/variable.js
@@ -0,0 +1,23 @@
+let name = 'rial';
+let age = 24;
+let isStudent = true;
+let isEmployee = false;
+
+console.log(name);
+console.log(typeof (name));
+console.log(age);
+console.log(typeof (age));
+console.log(isStudent);
+console.log(typeof (isStudent));
+console.log(isEmployee);
+console.log(typeof (isEmployee));
+
+
+let strNum = '35.99385993048';
+let floatNum = parseFloat(strNum);
+console.log(strNum);
+console.log(floatNum);
+let digit2Num = floatNum.toFixed(3);
+console.log(digit2Num);
+let intNum = parseInt(digit2Num);
+console.log(intNum);
